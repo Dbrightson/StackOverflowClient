10fbd8309e4c35cb336b09c0cdefe2933a9d2e2f
DON BRIGHTSON G <89180158+Dbrightson@users.noreply.github.com>
2023-08-01 01:27:54 +0530

    Update generateOTP.js

--- server/controllers/generateOTP.js
+++ server/controllers/generateOTP.js
@@ -1,39 +1,43 @@
 import OTP from '../models/otp'
 import User from '../models/auth'
-import sendOTP from '../utilities/messager'
+import sendOTP from '../utilities/messager';
 
 export const generateOTP = async (req, res) => {
     try {
-	    var user = await User.findOne({phno: req.body.phno})
-	    if (user) {
-	        console.log('server ctrl genOTP');
-	        if(!await OTP.findOne({phno: req.body.phno})){
-	            var otp = new OTP({
-	            phno: req.body.phno,
-	            otp: (Math.floor((Math.random()*10000)+1)).toString(),
-	            expiresIn: new Date().getTime() + 300*1000
-	            })
-	            await otp.save()
-	        } else {
-	            var otp = await OTP.findOneAndUpdate({phno: req.body.phno},{
-	                otp: Math.floor((Math.random()*10000)+1),
-	                expiresIn: new Date().getTime() + 300*1000
-	            },
-	            {returnDocument:'after'})
-	        }
-	        const otpStatus = sendOTP(otp)
-	        if (otpStatus){
-	            return res.status(200).send({message:'Mail Sent Successfully'})
-	        }else {
-	            return res.status(500).send({message:'Sending Mail failed'})
-	        }
-	    } else {
-	        return res.status(500).send({message:'User Not Found, Please register'})
-	    }
+        var user = await User.findOne({ phno: req.body.phno });
+        if (user) {
+            console.log('server ctrl genOTP');
+            let otp;
+            if (!await OTP.findOne({ phno: req.body.phno })) {
+                otp = new OTP({
+                    phno: req.body.phno,
+                    otp: (Math.floor(Math.random() * 10000) + 1).toString(),
+                    expiresIn: new Date().getTime() + 300 * 1000,
+                });
+                await otp.save();
+            } else {
+                otp = await OTP.findOneAndUpdate(
+                    { phno: req.body.phno },
+                    {
+                        otp: Math.floor(Math.random() * 10000) + 1,
+                        expiresIn: new Date().getTime() + 300 * 1000,
+                    },
+                    { returnDocument: 'after' }
+                );
+            }
+            const otpStatus = await sendOTP(otp); // Await the sendOTP function
+            if (otpStatus) {
+                return res.status(200).send({ message: 'Mail Sent Successfully' });
+            } else {
+                return res.status(500).send({ message: 'Sending Mail failed' });
+            }
+        } else {
+            return res.status(500).send({ message: 'User Not Found, Please register' });
+        }
     } catch (error) {
-        return res.status(500).send({message:error})
-	}
-}
+        return res.status(500).send({ message: error.message || 'Internal Server Error' });
+    }
+};
 
 export const verifyOTP = async (req, res) => {
     try {
