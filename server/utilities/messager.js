cc62316094750f9692f60c6bfa7799cb2bd0ac84
DON BRIGHTSON G <89180158+Dbrightson@users.noreply.github.com>
2023-08-01 02:19:28 +0530

    Update messager.js

--- server/utilities/messager.js
+++ server/utilities/messager.js
@@ -1,26 +1,28 @@
 import twilio from 'twilio';
-import dotenv from 'dotenv';
-dotenv.config();
+import dotenv from 'dotenv'
+dotenv.config()
 
 const accountSid = process.env.TWILIO_TOKEN;
 const authToken = process.env.AUTH_TOKEN;
 const client = twilio(accountSid, authToken);
 
-const sendOTP = async (user) => {
-  try {
-    const verification = await client.verify
-      .services(process.env.TWILIO_VERIFY_SERVICE_SID)
-      .verifications.create({
-        to: user.phno,
-        channel: 'sms',
-      });
-
-    console.log('OTP Send Success');
-    return true;
-  } catch (error) {
-    console.log('OTP Send failed', error);
-    return false;
-  }
-};
+const sendOTP = (user) => {
+  // console.log(user);
+  client.verify
+  .services(process.env.TWILI0_SERVICE)
+  .verifications.create({
+    to: user.phno,
+    channel: 'sms',
+  })
+  // .then(message => console.log(message.sid))
+    .then((verification) => {
+      console.log('OTP Send SUccess');
+      return true;
+    })
+    .catch((error) => {
+      console.log('OTP Send failed', error);
+      return false;
+    });
+}
 
 export default sendOTP;
