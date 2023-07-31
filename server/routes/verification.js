707a4f0b9a4402b55d2de7b6084c490d3ff5bb19
DON BRIGHTSON G <89180158+Dbrightson@users.noreply.github.com>
2023-08-01 00:37:54 +0530

    Update verification.js

--- server/routes/verification.js
+++ server/routes/verification.js
@@ -1,9 +1,34 @@
-import express from 'express'
-import { generateOTP, verifyOTP } from '../controllers/generateOTP'
+import twilio from 'twilio';
+import dotenv from 'dotenv';
+import express from 'express';
 
-const router = express.Router()
+const router = express.Router();
+dotenv.config();
 
-router.post('/phno', generateOTP)
-router.post('/otp', verifyOTP)
+const client = twilio(
+  process.env.TWILIO_TOKEN,
+  process.env.TWILIO_PASSWORD
+);
 
-export default router
+router.post('/verify/phno', (req, res) => {
+  const { phoneNumber, otp } = req.body;
+
+  client.verify
+    .services(process.env.TWILI0_SERVICE)
+    .verificationChecks.create({
+      to: phoneNumber,
+      code: otp,
+    })
+    .then((verificationCheck) => {
+      if (verificationCheck.status === 'approved') {
+        return res.status(200).send({ message: true });
+      } else {
+        return res.status(500).send({ message: 'Invalid OTP' });
+      }
+    })
+    .catch((error) => {
+      return res.status(500).send({ message: 'Retry' });
+    });
+});
+
+export default router;
