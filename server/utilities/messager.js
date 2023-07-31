a676e0936cf600ee639a48d87e1fe36a8365f8fa
DON BRIGHTSON G <89180158+Dbrightson@users.noreply.github.com>
2023-08-01 01:59:10 +0530

    Update messager.js

--- server/utilities/messager.js
+++ server/utilities/messager.js
@@ -2,13 +2,13 @@ import twilio from 'twilio';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
+const accountSid = process.env.TWILIO_TOKEN;
+const authToken = process.env.AUTH_TOKEN;
 const client = twilio(accountSid, authToken);
 
 const sendOTP = async (user) => {
   try {
-    const verification = await client.verify.services(process.env.TWILIO_VERIFY_SERVICE_SID)
+    const verification = await client.verify.services(process.env.TWILIO_SERVICE)
       .verifications
       .create({
         to: user.phno,
