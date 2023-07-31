9cf6e4606257cf1f59e6de747c417329c9282d22
DON BRIGHTSON G <89180158+Dbrightson@users.noreply.github.com>
2023-08-01 01:29:38 +0530

    Update messager.js

--- server/utilities/messager.js
+++ server/utilities/messager.js
@@ -7,22 +7,20 @@ const authToken = process.env.AUTH_TOKEN;
 const client = twilio(accountSid, authToken);
 
 const sendOTP = (user) => {
-  // console.log(user);
-  client.verify
-  .services(process.env.TWILI0_SERVICE)
-  .verifications.create({
-    to: user.phno,
-    channel: 'sms',
-  })
-  // .then(message => console.log(message.sid))
-    .then((verification) => {
-      console.log('OTP Send SUccess');
-      return true;
-    })
-    .catch((error) => {
-      console.log('OTP Send failed', error);
-      return false;
-    });
-}
+    return client.messages
+        .create({
+            body: user.otp + 'This is the OTP from Stackoverflow clone',
+            from: '+14175053515',
+            to: user.phno
+        })
+        .then(message => {
+            console.log('OTP Send Success');
+            return true;
+        })
+        .catch((error) => {
+            console.log('OTP Send failed', error);
+            return false;
+        });
+};
 
 export default sendOTP;
