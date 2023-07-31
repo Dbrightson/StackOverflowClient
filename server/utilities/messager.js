9c5520a0060e545c4d98c853833f5ecee4e2f7eb
DON BRIGHTSON G <89180158+Dbrightson@users.noreply.github.com>
2023-08-01 01:53:43 +0530

    Update messager.js

--- server/utilities/messager.js
+++ server/utilities/messager.js
@@ -1,26 +1,31 @@
 import twilio from 'twilio';
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from 'dotenv';
+dotenv.config();
 
-const accountSid = process.env.TWILIO_TOKEN;
-const authToken = process.env.AUTH_TOKEN;
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = twilio(accountSid, authToken);
 
-const sendOTP = (user) => {
-    return client.messages
-        .create({
-            body: user.otp + 'This is the OTP from Stackoverflow clone',
-            from: '+14175053515',
-            to: user.phno
-        })
-        .then(message => {
-            console.log('OTP Send Success');
-            return true;
-        })
-        .catch((error) => {
-            console.log('OTP Send failed', error);
-            return false;
-        });
+const sendOTP = async (user) => {
+  try {
+    const verification = await client.verify.services(process.env.TWILIO_VERIFY_SERVICE_SID)
+      .verifications
+      .create({
+        to: user.phno,
+        channel: 'sms',
+      });
+
+    if (verification.status === 'pending') {
+      console.log('OTP Send Success');
+      return true;
+    } else {
+      console.log('OTP Send failed');
+      return false;
+    }
+  } catch (error) {
+    console.log('Error sending OTP:', error);
+    return false;
+  }
 };
 
 export default sendOTP;
