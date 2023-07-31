929c78998d953b97e6f239c0e984eb540a873bb8
DON BRIGHTSON G <89180158+Dbrightson@users.noreply.github.com>
2023-08-01 02:17:41 +0530

    Update generateOTP.js

--- server/controllers/generateOTP.js
+++ server/controllers/generateOTP.js
@@ -1,10 +1,54 @@
-import mongoose from 'mongoose'
+import OTP from '../models/otp'
+import User from '../models/auth'
+import sendOTP from '../utilities/messager'
 
-const OTPSchema = mongoose.Schema({
-    email: { type: String, required: false },
-    phno: { type: String, required: true },
-    otp: { type: String, required: true },
-    expiresIn: { type: Date, required:true }
-})
+export const generateOTP = async (req, res) => {
+    try {
+	    var user = await User.findOne({phno: req.body.phno})
+	    if (user) {
+	        console.log('server ctrl genOTP');
+	        if(!await OTP.findOne({phno: req.body.phno})){
+	            var otp = new OTP({
+	            phno: req.body.phno,
+	            otp: (Math.floor((Math.random()*10000)+1)).toString(),
+	            expiresIn: new Date().getTime() + 300*1000
+	            })
+	            await otp.save()
+	        } else {
+	            var otp = await OTP.findOneAndUpdate({phno: req.body.phno},{
+	                otp: Math.floor((Math.random()*10000)+1),
+	                expiresIn: new Date().getTime() + 300*1000
+	            },
+	            {returnDocument:'after'})
+	        }
+	        const otpStatus = sendOTP(otp)
+	        if (otpStatus){
+	            return res.status(200).send({message:'Mail Sent Successfully'})
+	        }else {
+	            return res.status(500).send({message:'Sending Mail failed'})
+	        }
+	    } else {
+	        return res.status(500).send({message:'User Not Found, Please register'})
+	    }
+    } catch (error) {
+        return res.status(500).send({message:error})
+	}
+}
 
-export default mongoose.model('OTP', OTPSchema) 
+export const verifyOTP = async (req, res) => {
+    try {
+		var user = await OTP.findOne({ phno: req.body.phno })
+		if (user) {
+			if(req.body.recvOTP === user.otp){
+				return res.status(200).send({message:true})
+			}
+			else{
+				return res.status(500).send({message:false})
+			}
+		}else {
+			return res.status(500).send({message:'Retry'})
+		}
+    } catch (error) {
+    	return res.status(500).send({message:'Retry'+error})
+    }
+}
