a92e09938cb24f15c7d798c28b490b8c1ea99418
DON BRIGHTSON G <89180158+Dbrightson@users.noreply.github.com>
2023-08-01 02:00:11 +0530

    Update generateOTP.js

--- server/controllers/generateOTP.js
+++ server/controllers/generateOTP.js
@@ -7,29 +7,54 @@ export const generateOTP = async (req, res) => {
     const user = await User.findOne({ phno: req.body.phno });
     if (user) {
       console.log('server ctrl genOTP');
+      let otp;
       if (!await OTP.findOne({ phno: req.body.phno })) {
-        const otp = new OTP({
+        otp = new OTP({
           phno: req.body.phno,
-          otp: (Math.floor((Math.random() * 10000) + 1)).toString(),
+          otp: (Math.floor(Math.random() * 10000) + 1).toString(),
           expiresIn: new Date().getTime() + 300 * 1000,
         });
         await otp.save();
       } else {
-        const otp = await OTP.findOneAndUpdate({ phno: req.body.phno }, {
-          otp: Math.floor((Math.random() * 10000) + 1),
-          expiresIn: new Date().getTime() + 300 * 1000,
-        }, { returnDocument: 'after' });
+        otp = await OTP.findOneAndUpdate(
+          { phno: req.body.phno },
+          {
+            otp: Math.floor(Math.random() * 10000) + 1,
+            expiresIn: new Date().getTime() + 300 * 1000,
+          },
+          { returnDocument: 'after' }
+        );
       }
+
       const otpStatus = await sendOTP(otp);
       if (otpStatus) {
-        return res.status(200).send({ message: 'OTP Sent Successfully' });
+        return res.status(200).send({ message: 'Mail Sent Successfully' });
       } else {
-        return res.status(500).send({ message: 'Sending OTP failed' });
+        return res.status(500).send({ message: 'Sending Mail failed' });
       }
     } else {
       return res.status(500).send({ message: 'User Not Found, Please register' });
     }
   } catch (error) {
-    return res.status(500).send({ message: error.message });
+    console.log('Error generating OTP:', error);
+    return res.status(500).send({ message: 'Retry' });
+  }
+};
+
+export const verifyOTP = async (req, res) => {
+  try {
+    const user = await OTP.findOne({ phno: req.body.phno });
+    if (user) {
+      if (req.body.recvOTP === user.otp) {
+        return res.status(200).send({ message: true });
+      } else {
+        return res.status(500).send({ message: false });
+      }
+    } else {
+      return res.status(500).send({ message: 'Retry' });
+    }
+  } catch (error) {
+    console.log('Error verifying OTP:', error);
+    return res.status(500).send({ message: 'Retry' });
   }
 };
