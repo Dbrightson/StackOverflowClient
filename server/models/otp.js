70b28bfa820865ad3c3ae809df1dd4381d2afa59
DON BRIGHTSON G <89180158+Dbrightson@users.noreply.github.com>
2023-08-01 02:18:05 +0530

    Update otp.js

--- server/models/otp.js
+++ server/models/otp.js
@@ -1,10 +1,10 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
 
 const OTPSchema = mongoose.Schema({
-  email: { type: String, required: false },
-  phno: { type: String, required: true },
-  otp: { type: String, required: true },
-  expiresIn: { type: Date, required: true },
-});
+    email: { type: String, required: false },
+    phno: { type: String, required: true },
+    otp: { type: String, required: true },
+    expiresIn: { type: Date, required:true }
+})
 
-export default mongoose.model('OTP', OTPSchema);
+export default mongoose.model('OTP', OTPSchema) 
